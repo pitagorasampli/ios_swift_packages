7485fd67acf1c8bea3368b78957ebfb7f3d6d807
Julio Ribeiro <juliowebdeveloper@hotmail.com>
2024-09-12 20:36:40 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 let package = Package(
-   name: "Shared",
+   name: "ACO",
    platforms: [
      .iOS(.v14),
    ],
@@ -11,7 +11,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "aco",
-         url: "https://github.com/pitagorasampli/aco_mp/releases/download/0.0.12/aco.xcframework.zip",
-         checksum:"1448976fff5f8d16ae709c2856ead672dc7eae23c2feb6d0463a77ac0ff28da0")
+         url: "https://github.com/pitagorasampli/aco_mp/releases/download/0.0.13/aco.xcframework.zip",
+         checksum:"a4542e93a5d302792b6daf1dae6849d1981778600877123ed98702afc2562644")
    ]
 )
